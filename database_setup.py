1f46e651659d6c3b456fb60c98ba889c7e01c752
geordyp <geowilliams6@gmail.com>
2017-02-05 18:20:40 -0600

    Clean code

--- database_setup.py
+++ database_setup.py
@@ -11,9 +11,10 @@ import time
 
 Base = declarative_base()
 
+RESTuri = 'http://http://localhost:5000/ondeck/api/v1.0/'
 
 class EndUser(Base):
-    __tablename__ = "enduser"
+    __tablename__ = 'enduser'
 
     id = Column(String(250), primary_key=True)
     name = Column(String(250), nullable=False)
@@ -24,19 +25,19 @@ class EndUser(Base):
     @property
     def serialize(self):
         return {
-            "id": self.id,
-            "name": self.name,
-            "vision": self.vision,
-            "userURI": "http://138.197.77.126/ondeck/api/v1.0/user/" + self.id,
-            "createTaskURI": "http://138.197.77.126/ondeck/api/v1.0/tasks/new/" + self.id,
-            "doneTasksURI": "http://138.197.77.126/ondeck/api/v1.0/tasks/" + self.id + "/done",
-            "onDeckTasksURI": "http://138.197.77.126/ondeck/api/v1.0/tasks/" + self.id + "/on_deck",
-            "activeTasksURI": "http://138.197.77.126/ondeck/api/v1.0/tasks/" + self.id + "/active"
+            'id': self.id,
+            'name': self.name,
+            'vision': self.vision,
+            'userURI': RESTuri + 'user/' + self.id,
+            'createTaskURI': RESTuri + 'tasks/new/' + self.id,
+            'doneTasksURI': RESTuri + 'tasks/' + self.id + '/done',
+            'onDeckTasksURI': RESTuri + 'tasks/' + self.id + '/on_deck',
+            'activeTasksURI': RESTuri + 'tasks/' + self.id + '/active'
         }
 
 
 class Task(Base):
-    __tablename__ = "task"
+    __tablename__ = 'task'
 
     id = Column(String(250), primary_key=True)
     name = Column(String(250), nullable=False)
@@ -46,7 +47,7 @@ class Task(Base):
     done = Column(Boolean, nullable=False)
     completion_date = Column(Date, nullable=True)
     notes = Column(String(400), nullable=True)
-    enduser_id = Column(String(250), ForeignKey("enduser.id"), nullable=False)
+    enduser_id = Column(String(250), ForeignKey('enduser.id'), nullable=False)
     enduser = relationship(EndUser)
     date_created = Column(DateTime, default=datetime.datetime.utcnow)
 
@@ -57,17 +58,17 @@ class Task(Base):
         daysLeft = (due-now).days
 
         return {
-            "name": self.name,
-            "task_group": self.task_group,
-            "completion_date": str(self.completion_date) if self.completion_date else None,
-            "due_date": str(self.due_date),
-            "days_left": daysLeft,
-            "notes": self.notes,
-            "heads_up": str(self.heads_up) if self.heads_up else None,
-            "done": self.done,
-            "uri": "http://138.197.77.126/ondeck/api/v1.0/tasks/" + self.id
+            'name': self.name,
+            'task_group': self.task_group,
+            'completion_date': str(self.completion_date) if self.completion_date else None,
+            'due_date': str(self.due_date),
+            'days_left': daysLeft,
+            'notes': self.notes,
+            'heads_up': str(self.heads_up) if self.heads_up else None,
+            'done': self.done,
+            'uri': RESTuri + 'tasks/' + self.id
         }
 
 
-engine = create_engine("postgresql://me:password@localhost/tasks")
+engine = create_engine('postgresql://me:password@localhost/tasks')
 Base.metadata.create_all(engine)
